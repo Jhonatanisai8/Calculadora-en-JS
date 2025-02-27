cf039ce32799b7b62a3c7ac5b2c93abdc3aa39ed
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-02-27 16:16:12 -0500

    funciones para agregar y borrar de la pantalla pequeña

---
+++ script.js
@@ -0,0 +1,8 @@
+// agrega el valor del boton de la calculadora
+function agregar(valor) {
+    document.getElementById('pantalla').value += valor
+}
+
+function borrar() {
+    document.getElementById('pantalla').value = ""
+}
